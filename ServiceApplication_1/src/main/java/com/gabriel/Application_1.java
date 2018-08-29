194512516cc8ab392353226d9df34d215ddaced7
gaoxianghy <gaoxianghy@cmhi.chinamobile.com>
2018-08-29 09:35:26 +0800

    调整配置，新增一个module

--- ServiceApplication_1/src/main/java/com/gabriel/Application_1.java
+++ ServiceApplication_1/src/main/java/com/gabriel/Application_1.java
@@ -2,13 +2,14 @@ package com.gabriel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  *
  */
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application_1
 {
